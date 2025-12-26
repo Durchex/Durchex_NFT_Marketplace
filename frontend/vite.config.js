ecea88ff932c98871caf45e4d3be07e0aa00e3f6
proshielddev <proshieldlogisticsllc@gmail.com>
2025-12-26 02:18:48 +0100

    fix(vite): remove malformed comment and add production env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,27 +8,3 @@ export default defineConfig({
     "process.env": {},
   },
 });
-
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import path from 'path'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve( './src'),
-//     },
-//   },
-// })
