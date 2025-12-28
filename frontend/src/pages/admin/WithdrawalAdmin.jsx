0ffdd82a6a784edc8ff03faafad15897c99c668b
proshielddev <proshieldlogisticsllc@gmail.com>
2025-12-28 16:32:11 +0100

    Update WithdrawalAdmin.jsx

--- frontend/src/pages/admin/WithdrawalAdmin.jsx
+++ frontend/src/pages/admin/WithdrawalAdmin.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import toast from 'react-hot-toast';
-import { cartAPI } from '../services/api';
+import { cartAPI } from '../../services/api';
 import {
   FiArrowDownCircle,
   FiCheckCircle,
